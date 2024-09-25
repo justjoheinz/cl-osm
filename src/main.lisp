d2e3f9f994186c9992f0b7f4777ff023a4f6ed9b
Markus Klink <justjoheinz@gmail.com>
2024-09-25 14:13:34 +0200

    Add more docs

--- src/main.lisp
+++ src/main.lisp
@@ -42,92 +42,133 @@
                                  :url full-url))))))
 
 (defun api-versions ()
+  "Get the supported versions of the OpenStreetMap API."
   (let ((*osm-api-version* ""))
     (osm/get (format nil "versions"))))
 
 (defun api-capabilities ()
+  "Get the capabilities of the OpenStreetMap API."
   (osm/get (format nil "/capabilities")))
 
 (defun permissions ()
+  "Get the permissions of the logged-in user."
   (osm/get (format nil "/permissions")))
 
 (defun map-bbox (left bottom right top)
+  "Get map data for a bounding box."
   (osm/get (format nil "/map?bbox=~A,~A,~A,~A" left bottom right top)))
 
 ;; GET /api/0.6/[node|way|relation]/#id
 
 (defun node (id)
+  "Details of a node."
   (osm/get (format nil "/node/~A" id)))
 
 (defun way (id)
+  "Details of a way."
   (osm/get (format nil "/way/~A" id)))
 
 (defun relation (id)
+  "Details of a relation."
   (osm/get (format nil "/relation/~A" id)))
 
 ;; GET /api/0.6/[node|way|relation]/#id/history
 
 (defun node-history (id)
+  "History of a node."
   (osm/get (format nil "/node/~A/history" id)))
 
 (defun way-history (id)
+  "History of a way."
   (osm/get (format nil "/way/~A/history" id)))
 
 (defun relation-history (id)
+  "History of a relation."
   (osm/get (format nil "/relation/~A/history" id)))
 
 ;; GET /api/0.6/[node|way|relation]/#id/#version
 
 (defun node-version (id version)
+  "Details of a node at a specific version."
   (osm/get (format nil "/node/~A/~A" id version)))
 
 (defun way-version (id version)
+  "Details of a way at a specific version."
   (osm/get (format nil "/way/~A/~A" id version)))
 
 (defun relation-version (id version)
+  "Details of a relation at a specific version."
   (osm/get (format nil "/relation/~A/~A" id version)))
 
 ;; Relations for element: GET /api/0.6/[node|way|relation]/#id/relations
 
 (defun node-relations (id)
+  "Relations that include a node."
   (osm/get (format nil "/node/~A/relations" id)))
 
 (defun way-relations (id)
+  "Relations that include a way."
   (osm/get (format nil "/way/~A/relations" id)))
 
 (defun relation-relations (id)
+  "Relations that include a relation."
   (osm/get (format nil "/relation/~A/relations" id)))
 
 ;; Ways for node: GET /api/0.6/node/#id/ways
 
 (defun node-ways (id)
+  "Ways that include a node."
   (osm/get (format nil "/node/~A/ways" id)))
 
 ;; Full: GET /api/0.6/[way|relation]/#id/full
 
 (defun way-full (id)
+  "Full details of a way."
   (osm/get (format nil "/way/~A/full" id)))
 
 (defun relation-full (id)
+  "Full details of a relation."
   (osm/get (format nil "/relation/~A/full" id)))
 
 ;; Multi fetch: GET /api/0.6/[nodes|ways|relations]?#parameters
 
 (defun nodes (ids)
+  "Details of multiple nodes."
   (osm/get (format nil "/nodes?nodes=~{~A~^,~}" ids)))
 
 (defun ways (ids)
+  "Details of multiple ways."
   (osm/get (format nil "/ways?ways=~{~A~^,~}" ids)))
 
 (defun relations (ids)
+  "Details of multiple relations."
   (osm/get (format nil "/relations?relations=~{~A~^,~}" ids)))
 
 ;; Changesets: GET /api/0.6/changeset/#id
 
 (defun changeset (id &key (include-discussion nil))
+  "Details of a changeset. If INCLUDE-DISCUSSION is true, include discussion comments."
   (if include-discussion
       (osm/get (format nil "/changeset/~A?include_discussion=true" id))
       (osm/get (format nil "/changeset/~A" id))))
+
+;; Methods for user data
+
+(defun user (id)
+  "Details of a user."
+  (osm/get (format nil "/user/~A" id)))
+
+(defun users (ids)
+  "Details of multiple users."
+  (osm/get (format nil "/users?users=~{~A~^,~}" ids)))
+
+(defun user-details ()
+  "Details of the logged-in user."
+  (osm/get "/user/details"))
+
+(defun preferences ()
+  "Preferences of the logged-in user."
+  (osm/get "/preferences"))
 
 ;; create a parameter *oauth-ojb* and assign the return value
 (defparameter *oauth-obj* nil)
